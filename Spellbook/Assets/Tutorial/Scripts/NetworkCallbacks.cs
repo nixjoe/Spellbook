0bb6159c05764941fbae687f3d98b4df0dd80062
moicompsci <moi.compsci@gmail.com>
2019-01-27 09:31:34 -0800

    Photon Bolt init

---
+++ Spellbook/Assets/Tutorial/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ The Bolt.GlobalEventListener class itself does inherit 
+ from MonoBehaviour so you can do all the normal Unity 
+ stuff as usual, but it adds several Bolt specific methods on-top.
+     */
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        //base.SceneLoadLocalDone(scene);
+
+        // randomize a position
+        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+    }
+}

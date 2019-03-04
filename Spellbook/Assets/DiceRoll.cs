7eb58b23c51bd4e319028b14a27576dcd268c41c
jan-yu <jan.yu415@gmail.com>
2019-03-04 02:48:52 -0800

    added simple dice roll and made the inventory less ugly by adding masking.

---
+++ Spellbook/Assets/DiceRoll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoll : MonoBehaviour
+{
+    public GameObject TextBox;
+    public int RandomNumber;
+
+    public void NumberGenerator() 
+    {
+        RandomNumber = Random.Range(1, 7);
+        TextBox.GetComponent<Text>().text = "" + RandomNumber;
+    }
+}
---
+++ Spellbook/Assets/Scripts/RandomNumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomNumberGenerator : MonoBehaviour
+{
+    public GameObject TextBox;
+    public int RandomNumber;
+
+    // Start is called before the first frame update
+    public void NumberGenerator()
+    {
+        RandomNumber = Random.Range(1, 6);
+        TextBox.GetComponent<Text>().text = "" + RandomNumber;
+    }
+
+}
